d435c1dd39bc42d6a26e92c7d5b017f9cd51c392
lucasjkr <lucas@lucaskrupinski.com>
2019-01-29 12:29:20 -0500

    Update update.sh
    update for container name;
    also update composer dependencies

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-docker exec php php artisan down --message="update in progress"
-docker exec php git pull
-docker exec php php artisan migrate --force
-docker exec php php artisan up
+docker exec snipe_php php artisan down --message="update in progress"
+docker exec snipe_php git pull
+docker exec snipe_php composer update
+docker exec snipe_php php artisan migrate --force
+docker exec snipe_php php artisan up
